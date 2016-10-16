213e97a0a693688c52956804467f015318def980
Masuhara <Masuhara@Masuhara-Air.local>
2016-10-16 22:19:51 +0900

    Complete ver.0.1

--- Gacha/ViewController.swift
+++ Gacha/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 変数の宣言
+    var number = 0
+    @IBOutlet weak var monsterImageView: UIImageView!
+    @IBOutlet weak var gachaButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 初期設定
+        monsterImageView.image = nil
+        gachaButton.layer.cornerRadius = gachaButton.bounds.height / 2
+        gachaButton.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapGachaButton() {
+        
+        // ガチャのアルゴリズム
+        number = Int(arc4random_uniform(100))
+        if number < 60 {
+            monsterImageView.image = UIImage(named: "kamakiri@2x.png")
+        } else if number >= 60 && number < 80 {
+            monsterImageView.image = UIImage(named: "man@2x.png")
+        } else {
+            monsterImageView.image = UIImage(named: "woman@2x.png")
+        }
+        
+        // アニメーション
+        monsterImageView.alpha = 0.0
+        UIView.animate(withDuration: 1.0) { 
+            self.monsterImageView.alpha = 1.0
+        }
+    }
 }
 
